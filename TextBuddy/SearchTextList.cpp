1b8d18d0ad57ed53433e49a856c4599ee78c8603
Evan Sebastian <evanlhoini@gmail.com>
2014-09-17 15:26:18 +0800

    [TDD] Fix failing case on SearchTextList

--- TextBuddy/SearchTextList.cpp
+++ TextBuddy/SearchTextList.cpp
@@ -8,7 +8,27 @@ const std::string SearchTextList::KEYWORD_NOT_FOUND = "Keyword not found\n";
 /// with one non empty string as a keyword, separated by single space.
 /// \param [command] command to be interpreted.
 bool SearchTextList::interpret(const App::Command& command) {
-	return true;
+	std::istringstream inString(command);
+	std::string commandKeyword;
+	std::string searchKeyword;
+	std::string dummy;
+
+	// Consume one word from the string, return false if not match.
+	inString >> commandKeyword;
+	bool keywordNotMatch = commandKeyword != SEARCH_KEYWORD;
+	if (keywordNotMatch) {
+		return false;
+	}
+
+	// Consume one word as a search keyword, if there is no more
+	// word or more than one words, return false
+	inString >> searchKeyword;
+	if (!(inString >> dummy)) {
+		inputCache = searchKeyword;
+		return true;
+	} else {
+		return false;
+	}
 }
 
 /// Search the occurence of the word inputCache in the text list.
